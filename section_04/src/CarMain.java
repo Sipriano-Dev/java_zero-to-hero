99a6509a4a78f391e683f7bbb724f2b982f428a9
Anderson Sipriano <Andsipriano@gmail.com>
2024-05-06 14:17:03 -0300

    Demo of Heap memory and stack memory in IntelliJ

--- section_04/src/CarMain.java
+++ section_04/src/CarMain.java
@@ -1,18 +1,14 @@
 public class CarMain {
+
+    static {
+        System.out.println("Static block from the same class");
+    }
+
     public static void main(String[] args) {
+
         System.out.println("Main method started...");
-
         Car car1 = new Car();
-        System.out.println(car1.model);
-        System.out.println(car1.color);
-        System.out.println(car1.horsePower);
-
         Car car2 = new Car("Civic", "Black", 200);
-        System.out.println(car2.model);
-        System.out.println(car2.color);
-        System.out.println(car2.horsePower);
-
-        System.out.println(Car.NU_OF_WHEELS);
         System.out.println(Car.nuOfEngines);
 
     }
