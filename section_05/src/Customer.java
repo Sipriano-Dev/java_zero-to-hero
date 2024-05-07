6a6a5bbf52f7ada7a89b48bb6168e09b5ff3f1d5
Anderson Sipriano <Andsipriano@gmail.com>
2024-05-07 16:00:45 -0300

    Multi line Comments in Java - Demo

---
+++ section_05/src/Customer.java
@@ -0,0 +1,7 @@
+/*
+This class represents a customer in bank apps.
+It contains information such as first name
+last name, DOB, address and account details
+ */
+public class Customer {
+}
---
+++ section_05/src/MultilineCommentDemo.java
@@ -0,0 +1,29 @@
+public class MultilineCommentDemo {
+
+    /*
+    Based on business requirements, the end user
+    will be allowed to retry a maximum of 3 login
+    attempts. Using the below constant, the same is
+    going to be controlled. If the requirement changes
+    , then change the bellow values.
+     */
+    static final int MAX_RETRY_ATTEMPTS = 3;
+
+    public static void main(String[] args) {
+        /*MultilineCommentDemo obj = new MultilineCommentDemo();
+        double result = obj.calculateRectangleArea(24, 34);
+        System.out.println(result);*/
+    }
+
+    /*
+    This method calculates the area of the rectangle based
+    on given length and width. The logic it is going to have
+    is multiply two given input method arguments and returns
+    the same to the caller.
+     */
+
+    public double calculateRectangleArea(double length, double width) {
+        return length * width;
+    }
+
+}
