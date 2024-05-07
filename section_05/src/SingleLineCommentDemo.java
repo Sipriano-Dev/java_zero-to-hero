fe18c1d8dcf0b900d6925e84b67beb41ba036d2a
Anderson Sipriano <Andsipriano@gmail.com>
2024-05-07 15:29:51 -0300

    Single line Comments in Java - Demo

---
+++ section_05/src/SingleLineCommentDemo.java
@@ -0,0 +1,23 @@
+public class SingleLineCommentDemo {
+
+    int age; //Represent the age of the customer
+
+    //Maximum numbers of login attempts allowed
+    static final int MAX_RETRY_ATTEMPTS = 3;
+
+    public static void main(String[] args) {
+
+        //y indicates yes
+        char y = 'Y';
+        //n indicates no
+        char n = 'N';
+
+        //sum(2, 5);
+
+    }
+
+    public static void sum(int num1, int num2) {
+        System.out.println(num1 + num2);
+    }
+
+}
