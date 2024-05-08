fffb5349a4edc4d2ba6b78b4145e0320122e9621
Anderson Sipriano <Andsipriano@gmail.com>
2024-05-08 15:43:53 -0300

    Escape sequence character & Unicode char values in String

---
+++ section_06/src/ScapeSeqDemo.java
@@ -0,0 +1,14 @@
+public class ScapeSeqDemo {
+
+    public static void main(String[] args) {
+        String name = "\"Anderson\"";//Scape sequence character
+        System.out.println(name);
+
+        String m = "\u004D"; //M
+        System.out.println(m);
+        String unicodeName = "\u004Dadan\u0021";//Can mix unicode
+        System.out.println(unicodeName);
+
+    }
+
+}
