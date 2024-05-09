9e6be37da07a4646a550e38e39f086bea75ba299
Anderson Sipriano <Andsipriano@gmail.com>
2024-05-09 13:26:56 -0300

    Fetching a character at an index in String

---
+++ section_06/src/CharAtMethodDemo.java
@@ -0,0 +1,9 @@
+public class CharAtMethodDemo {
+
+    public static void main(String[] args) {
+        String java = "Java";
+        char j = java.charAt(0); //J
+        char exception = java.charAt(4); //OutOfBound Exception
+    }
+
+}
