811fedcce3e371797afbee82d499b40651bac1f3
Anderson Sipriano <Andsipriano@gmail.com>
2024-05-11 12:51:52 -0300

    Addition Operator in Java

---
+++ section_07/src/ArithmeticOperatorsDemo.java
@@ -0,0 +1,18 @@
+public class ArithmeticOperatorsDemo {
+
+    public static void main(String[] args) {
+        byte num1;
+        byte num2 = 5;
+        byte num3 = 3;
+        num1 = (byte) (num2 + num3);
+        num1 = 5 + 3;
+
+        double num4;
+        double num5 = 3.14;
+        byte num6 = 3;
+        num4 =  num5 + num6;
+        System.out.println(num4);
+        
+    }
+
+}
