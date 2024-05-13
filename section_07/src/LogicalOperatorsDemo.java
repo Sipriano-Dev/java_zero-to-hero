c86462a0a8ec147b760b6c78ff35c3e7d6fe3fd8
Anderson Sipriano <Andsipriano@gmail.com>
2024-05-13 15:19:18 -0300

    Logical Short-Circuit AND, Logical AND operators in Java - Demo

--- section_07/src/LogicalOperatorsDemo.java
+++ section_07/src/LogicalOperatorsDemo.java
@@ -8,7 +8,11 @@ public class LogicalOperatorsDemo {
         int num1 = 9;
         int num2 = 6;
         isValid = ! (num1>num2);
-        
+
+        boolean a = (num1>10) && ((num2=16)>15);
+        System.out.println(a);
+        System.out.println(num2);
+
     }
 
 }
