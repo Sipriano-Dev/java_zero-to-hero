ef8cfed170521cd6487ae11fc36177cc1ab5b422
Anderson Sipriano <Andsipriano@gmail.com>
2024-05-13 17:20:06 -0300

    Bitwise AND operator in Java

--- section_07/src/BitwiseOperatorsDemo.java
+++ section_07/src/BitwiseOperatorsDemo.java
@@ -5,7 +5,11 @@ public class BitwiseOperatorsDemo {
         int x = 9;
         int y = ~x; // -10
         System.out.println(y);
-        
+
+        int x1 = 10;
+        int y1 = 6;
+        int z1 = x1 & y1; // 2
+        System.out.println(z1);
 
     }
 
