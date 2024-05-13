e0c05cf0d5ca20d11c3e9403272887c731c377e9
Anderson Sipriano <Andsipriano@gmail.com>
2024-05-13 15:01:39 -0300

    Logical NOT Operator in Java

---
+++ section_07/src/LogicalOperatorsDemo.java
@@ -0,0 +1,14 @@
+public class LogicalOperatorsDemo {
+
+    public static void main(String[] args) {
+
+        boolean isValid;
+        isValid = !true;
+        isValid = !false;
+        int num1 = 9;
+        int num2 = 6;
+        isValid = ! (num1>num2);
+        
+    }
+
+}
