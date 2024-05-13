927d2ee19ac74a296d50e79b35fefbd789551bed
Anderson Sipriano <Andsipriano@gmail.com>
2024-05-13 17:11:44 -0300

    Bitwise NOT operator or 1’s complement in Java

---
+++ section_07/src/BitwiseOperatorsDemo.java
@@ -0,0 +1,12 @@
+public class BitwiseOperatorsDemo {
+
+    public static void main(String[] args) {
+
+        int x = 9;
+        int y = ~x; // -10
+        System.out.println(y);
+        
+
+    }
+
+}
