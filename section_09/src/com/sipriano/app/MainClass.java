5cc515774af40e2855166618f81e545a6d104090
Anderson Sipriano <Andsipriano@gmail.com>
2024-05-16 16:11:23 -0300

    The static import statements

--- section_09/src/com/sipriano/app/MainClass.java
+++ section_09/src/com/sipriano/app/MainClass.java
@@ -1,7 +1,10 @@
 package com.sipriano.app;
 
 import com.sipriano.model.Employee;
-import com.sipriano.model.Vehicle; //Qualified name, not need to write it all in every use
+import com.sipriano.model.Vehicle;
+
+import com.sipriano.utility.MyConstants;
+import static java.lang.Math.PI;
 
 public class MainClass {
 
@@ -12,6 +15,13 @@ public class MainClass {
 
         String str = new String("Hello"); //all class in java.lang doesn't need import
         Integer num;
+
+        System.out.println(MyConstants.SHIPPING_COST);
+        System.out.println(MyConstants.TAX_RATE);
+
+        System.out.println(MyConstants.calculateTotalCost(9.99));
+
+        System.out.println(PI);
 
     }
 
