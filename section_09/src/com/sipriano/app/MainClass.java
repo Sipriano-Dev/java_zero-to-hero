a9e4afe4c6e6d922d8fe1377965700a3d5c4cc7d
Anderson Sipriano <Andsipriano@gmail.com>
2024-05-16 15:21:01 -0300

    Using package members with import statement

---
+++ section_09/src/com/sipriano/app/MainClass.java
@@ -0,0 +1,15 @@
+package com.sipriano.app;
+
+import com.sipriano.model.Employee;
+import com.sipriano.model.Vehicle; //Qualified name, not need to write it all in every use
+
+public class MainClass {
+
+    public static void main(String[] args) {
+
+        Vehicle vehicle = new Vehicle();
+        Employee employee = new Employee();
+
+    }
+
+}
