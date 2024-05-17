96f7e4d7afd17778dfecf306be75e7b50ea6f26d
Anderson Sipriano <Andsipriano@gmail.com>
2024-05-17 16:08:09 -0300

    What a subclass inherits from its superclass

--- section_10/src/com/sipriano/marvel/base/Person.java
+++ section_10/src/com/sipriano/marvel/base/Person.java
@@ -4,6 +4,11 @@ public class Person {
 
     protected String name;
     protected int age;
+    protected static int nuOfHands = 2;
+
+    protected void calculateAge(int year) {
+        System.out.println("Age method");
+    }
 
     public void eat(String food) {
         System.out.println("Person is eating " + food);
