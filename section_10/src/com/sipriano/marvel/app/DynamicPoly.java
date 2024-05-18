2407880bb1b8bfc2b872d49ffd7cf99769f30051
Anderson Sipriano <Andsipriano@gmail.com>
2024-05-18 15:24:13 -0300

    Demo of dynamic polymorphism using method overriding

---
+++ section_10/src/com/sipriano/marvel/app/DynamicPoly.java
@@ -0,0 +1,13 @@
+package com.sipriano.marvel.app;
+
+import com.sipriano.marvel.base.Person;
+import com.sipriano.marvel.heroes.IronMan;
+
+public class DynamicPoly {
+
+    public static void main(String[] args) {
+        Person person = new IronMan(); //Upcasting
+        person.walk(); //IronMan method will be invoked
+    }
+
+}
