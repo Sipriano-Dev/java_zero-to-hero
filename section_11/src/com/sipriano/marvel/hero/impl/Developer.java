8decb916d7ff57a0d5f4207cfffffc2744ad9b64
Anderson Sipriano <Andsipriano@gmail.com>
2024-05-21 16:34:14 -0300

    Multiple Inheritance using interfaces - Part 2

---
+++ section_11/src/com/sipriano/marvel/hero/impl/Developer.java
@@ -0,0 +1,6 @@
+package com.sipriano.marvel.hero.impl;
+
+import com.sipriano.marvel.hero.Person;
+
+public class Developer extends Employee implements Person {
+}
---
+++ section_11/src/com/sipriano/marvel/hero/impl/Employee.java
@@ -0,0 +1,9 @@
+package com.sipriano.marvel.hero.impl;
+
+public class Employee {
+
+    public String walk() {
+        return "Employee Walking";
+    }
+
+}
