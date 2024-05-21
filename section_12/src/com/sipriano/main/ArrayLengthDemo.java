57616bc3981d4f7555c746b7334caa4e60994e75
Anderson Sipriano <Andsipriano@gmail.com>
2024-05-21 18:12:38 -0300

    Arrays length

---
+++ section_12/src/com/sipriano/main/ArrayLengthDemo.java
@@ -0,0 +1,15 @@
+package com.sipriano.main;
+
+public class ArrayLengthDemo {
+
+    public static void main(String[] args) {
+
+        int[] nums = {1, 2, 3, 4, 5};
+        int[] mobileNumbers = new int[101];
+
+        System.out.println(nums.length);
+        System.out.println(mobileNumbers.length);
+
+    }
+
+}
