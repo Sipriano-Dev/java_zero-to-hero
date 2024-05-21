2bf2ffd2fcb3590d13feb5995efb815130f883a8
Anderson Sipriano <Andsipriano@gmail.com>
2024-05-21 18:04:10 -0300

    Declaring & initializing Arrays

---
+++ section_12/src/com/sipriano/main/ArrayBasics.java
@@ -0,0 +1,22 @@
+package com.sipriano.main;
+
+import com.sipriano.model.Person;
+
+public class ArrayBasics {
+
+    public static void main(String[] args) {
+
+        int[] mobileNumbers = new int[5];
+        mobileNumbers[0] = 948385838;
+        mobileNumbers[1] = 984838593;
+        mobileNumbers[2] = 938537583;
+        mobileNumbers[3] = 958395837;
+        mobileNumbers[4] = 947383583;
+
+        int[] numbers = {1, 2, 3, 4, 5};
+
+        int length = 100;
+        Person[] person = new Person[length*2];
+    }
+
+}
