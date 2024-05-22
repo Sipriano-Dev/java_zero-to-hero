d2bc88e8ed30cee90700b154b347a9ec70b29d1c
Anderson Sipriano <Andsipriano@gmail.com>
2024-05-22 19:03:33 -0300

    Copying Arrays using arraycopy and copyOf methods

--- section_12/src/com/sipriano/main/ArrayCopyDemo.java
+++ section_12/src/com/sipriano/main/ArrayCopyDemo.java
@@ -1,5 +1,8 @@
 package com.sipriano.main;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class ArrayCopyDemo {
 
     public static void main(String[] args) {
@@ -7,9 +10,12 @@ public class ArrayCopyDemo {
         int[] oldArray = {1, 2, 3, 4, 5};
         int[] newArray = new int[oldArray.length + 5];
 
-        copyArrayUsingLoop(oldArray, newArray);
+        //copyArrayUsingLoop(oldArray, newArray);
+        System.arraycopy(oldArray, 0, newArray, 0, oldArray.length);
+        int[] targetArray = Arrays.copyOf(oldArray, oldArray.length + 1);
+        int[] targetArray1 = Arrays.copyOfRange(oldArray, 1, 4);
 
-        for (int num : newArray) {
+        for (int num : targetArray1) {
             System.out.println(num);
         }
 
