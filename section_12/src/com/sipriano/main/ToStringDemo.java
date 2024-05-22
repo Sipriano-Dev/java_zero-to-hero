341d119b4e547fa159f3c7bdf504333ccbe70ee9
Anderson Sipriano <Andsipriano@gmail.com>
2024-05-22 19:10:33 -0300

    Converting Arrays

---
+++ section_12/src/com/sipriano/main/ToStringDemo.java
@@ -0,0 +1,15 @@
+package com.sipriano.main;
+
+import java.util.Arrays;
+
+public class ToStringDemo {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {1, 2, 3, 4, 5};
+
+        System.out.println(Arrays.toString(numbers));
+
+    }
+
+}
