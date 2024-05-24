3e28c86646eef6c322cd87479c5c0f7a88089aea
Anderson Sipriano <Andsipriano@gmail.com>
2024-05-24 15:55:39 -0300

    BufferedReader examples to identify even number and sum

---
+++ section_13/src/com/sipriano/input/BufferedReaderEvenSumDemo.java
@@ -0,0 +1,27 @@
+package com.sipriano.input;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BufferedReaderEvenSumDemo {
+
+    public static void main(String[] args) throws IOException {
+
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader bf = new BufferedReader(isr);
+
+        System.out.println("Please enter a first numeric value...");
+        String input1 = bf.readLine();
+        System.out.println("Please enter a first numeric value...");
+        String input2 = bf.readLine();
+
+
+        int num1 = Integer.parseInt(input1);
+        int num2 = Integer.parseInt(input2);
+
+        System.out.println("The Sum is : " + (num1 + num2));
+
+    }
+
+}
