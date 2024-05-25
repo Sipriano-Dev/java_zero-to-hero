05f4bcff6f313bd32d67dad51ca89e4296274737
Anderson Sipriano <Andsipriano@gmail.com>
2024-05-25 15:43:35 -0300

    Logging in Java - Part 2

--- section_13/src/com/sipriano/log/LoggingDemo.java
+++ section_13/src/com/sipriano/log/LoggingDemo.java
@@ -9,8 +9,12 @@ public class LoggingDemo {
 
     public static void main(String[] args) {
 
+        logger.setLevel(Level.WARNING);
         logger.info("This is info level logging");
-        logger.log(Level.INFO, "This is info level logging");
+        logger.log(Level.WARNING, "This is warning level logging");
+        logger.severe("This is severe logging");
+
+        System.out.println("Hello using System.out.println");
 
     }
 
