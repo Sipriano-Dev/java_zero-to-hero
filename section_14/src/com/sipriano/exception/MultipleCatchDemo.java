9e0eab4bc4d124103872687dd45a3d22896d57f2
Anderson Sipriano <Andsipriano@gmail.com>
2024-05-25 17:43:45 -0300

    [JAVA 7] multiple catch blocks - Part 2

--- section_14/src/com/sipriano/exception/MultipleCatchDemo.java
+++ section_14/src/com/sipriano/exception/MultipleCatchDemo.java
@@ -18,7 +18,7 @@ public class MultipleCatchDemo {
             logger.info(str);
         } catch (NullPointerException e) {
             logger.severe("An null pointer exception occurred. Please check your data");
-        } catch (StringIndexOutOfBoundsException e) {
+        } catch (StringIndexOutOfBoundsException | ArrayIndexOutOfBoundsException e) {
             logger.severe("IndexOutOfBoundsException exception occurred. Please check your input data");
         } catch (Exception e) {
             logger.severe("Exception exception occurred. Please check your input data");
