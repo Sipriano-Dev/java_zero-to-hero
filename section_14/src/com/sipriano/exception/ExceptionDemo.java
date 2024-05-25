d042e9d388c38b126d05c37972973a27b852e428
Anderson Sipriano <Andsipriano@gmail.com>
2024-05-25 16:42:48 -0300

    try-catch block - Part 1

--- section_14/src/com/sipriano/exception/ExceptionDemo.java
+++ section_14/src/com/sipriano/exception/ExceptionDemo.java
@@ -1,17 +1,22 @@
 package com.sipriano.exception;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ExceptionDemo {
 
     public static void main(String[] args) {
 
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Enter a number: ");
-        int num = scan.nextInt();
-        System.out.println("You typed the number " + num);
-
-        scan.close();
+        try {
+            Scanner scan = new Scanner(System.in);
+            System.out.print("Enter a number: ");
+            int num = scan.nextInt();
+            System.out.println("You typed the number " + num);
+            scan.close();
+        } catch (InputMismatchException e) {
+            System.out.println("Provide a numerical and try again");
+            e.
+        }
 
     }
 
