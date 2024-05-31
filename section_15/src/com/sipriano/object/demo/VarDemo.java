858b461189bcfd4ab49381711315850be2d3ef78
Anderson Sipriano <Andsipriano@gmail.com>
2024-05-30 22:05:29 -0300

    [JAVA 10] var (local variable type inference) - Part 1

---
+++ section_15/src/com/sipriano/object/demo/VarDemo.java
@@ -0,0 +1,11 @@
+package com.sipriano.object.demo;
+
+public class VarDemo {
+
+    public static void main(String[] args) {
+        var message = "Hello World !";
+        var person = new Person();
+        var arr = new ArrayIndexOutOfBoundsException();
+    }
+
+}
