cc547b837fca230e98193980e419cc1a7fe380a7
Anderson Sipriano <Andsipriano@gmail.com>
2024-05-30 21:54:00 -0300

    [JAVA 16] Record classes - Part 2

--- section_15/src/com/sipriano/object/demo/RecordDemo.java
+++ section_15/src/com/sipriano/object/demo/RecordDemo.java
@@ -1,14 +1,6 @@
 package com.sipriano.object.demo;
 
-/**
- * 1) making the class as final
- * 2) make the fields as final and private
- * 3) generate the getter methods without any get prefix
- * 4) Generate a constructor with the fields mentioned inside the record header
- * 5) toString, equals and hashcode
- * @param name
- * @param occupation
- */
+import java.lang.reflect.RecordComponent;
 
 public class RecordDemo {
 
@@ -18,6 +10,13 @@ public class RecordDemo {
 
         System.out.println(person.name());
         System.out.println(person.occupation());
+
+        System.out.println(person.getClass().isRecord());
+
+        RecordComponent[] recordComponents = person.getClass().getRecordComponents();
+        for (RecordComponent r: recordComponents) {
+            System.out.println(r.toString());
+        }
 
     }
 
