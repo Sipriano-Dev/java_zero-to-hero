235dc328bca57ae2756e8b7abef2cfc03b98856a
Anderson Sipriano <Andsipriano@gmail.com>
2024-05-31 20:34:10 -0300

    How to create objects of ArrayList

---
+++ section_17/src/com/sipriano/list/ArrayListDemo.java
@@ -0,0 +1,17 @@
+package com.sipriano.list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListDemo {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> nums = new ArrayList<Integer>();
+        List<String> countryNames = new ArrayList<String>();
+        List<Character> characters = new ArrayList<Character>();
+        var doubleNums = new ArrayList<Double>();
+
+
+    }
+
+}
