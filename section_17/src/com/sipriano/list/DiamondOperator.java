b0bf02ce5516d26e9c175f0490ecfd7afebac4ef
Anderson Sipriano <Andsipriano@gmail.com>
2024-06-01 18:23:59 -0300

    [JAVA 7] Diamond Operator in Java

---
+++ section_17/src/com/sipriano/list/DiamondOperator.java
@@ -0,0 +1,15 @@
+package com.sipriano.list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DiamondOperator {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> nums = new ArrayList<>();
+        List<Character> characters = new ArrayList<>();
+        var doubleNums = new ArrayList<Double>(); 
+        List<String> countryNames = new ArrayList<>();
+    }
+
+}
