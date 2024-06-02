0447f332d856f8cc965819d2c2e01f9c2c0018e0
Anderson Sipriano <Andsipriano@gmail.com>
2024-06-01 21:53:30 -0300

    Sorting ArrayList using sort() methods

---
+++ section_17/src/com/sipriano/list/SortingDemo.java
@@ -0,0 +1,36 @@
+package com.sipriano.list;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class SortingDemo {
+
+    public static void main(String[] args) {
+
+        List<Integer> list = Arrays.asList(35, 63, 6, 0, 3, -12, 20);
+        System.out.println(list);
+        System.out.println("---------------");
+
+        Collections.sort(list);
+        System.out.println(list);
+        Collections.sort(list, Comparator.reverseOrder());
+        System.out.println(list);
+        System.out.println("___________________________");
+
+        var stringList = Arrays.asList("Anderson", "Gabrielle", "Felipe");
+        Collections.sort(stringList);
+        System.out.println(stringList);
+        Collections.sort(stringList, Comparator.reverseOrder());
+        System.out.println(stringList);
+        System.out.println("____________________________");
+
+        list.sort(Comparator.reverseOrder());
+        System.out.println(list);
+        list.sort(Comparator.naturalOrder());
+        System.out.println(list);
+
+    }
+
+}
