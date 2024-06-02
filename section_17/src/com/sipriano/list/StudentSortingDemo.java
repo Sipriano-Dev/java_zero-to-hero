aabf191531886074afcd0f15083e88022294f537
Anderson Sipriano <Andsipriano@gmail.com>
2024-06-02 00:41:12 -0300

    Sorting ArrayList of custom data type using Comparable

---
+++ section_17/src/com/sipriano/list/StudentSortingDemo.java
@@ -0,0 +1,31 @@
+package com.sipriano.list;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class StudentSortingDemo {
+
+    public static void main(String[] args) {
+
+        var students = new ArrayList<Student>();
+        students.add(new Student("John", 245, 85));
+        students.add(new Student("Joana", 246, 90));
+        students.add(new Student("Suzy", 247, 83));
+        System.out.println(students);
+        System.out.println("__________________________");
+
+        Collections.sort(students);
+        System.out.println(students);
+        Collections.sort(students.reversed());
+        System.out.println(students);
+        System.out.println("-------------------------");
+        students.sort(Comparator.naturalOrder());
+        System.out.println(students);
+        students.sort(Comparator.reverseOrder());
+
+
+
+    }
+
+}
