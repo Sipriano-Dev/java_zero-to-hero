7c67a57f9f2a8db31de13361ae3635aa0f72f5fa
Anderson Sipriano <Andsipriano@gmail.com>
2024-06-03 17:13:45 -0300

    How to create objects of LinkedList

---
+++ section_18/src/com/sipriano/list/LinkedListDemo.java
@@ -0,0 +1,20 @@
+package com.sipriano.list;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class LinkedListDemo {
+
+    public static void main(String[] args) {
+        LinkedList<Integer> nums = new LinkedList<>();
+        List<Double> doubleNums = new LinkedList<>();
+        var countryNames = new LinkedList<String>();
+        countryNames.add("India");
+        countryNames.add("Canada");
+        countryNames.add("USA");
+        countryNames.add("Germany");
+        countryNames.add("India");
+
+    }
+
+}
