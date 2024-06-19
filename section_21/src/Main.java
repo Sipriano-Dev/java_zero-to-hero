982e00f6aa4a4348d0de20fb2923cbc7c8814827
Anderson Sipriano <Andsipriano@gmail.com>
2024-06-19 19:45:15 -0300

    Exercise Vowel Remove using HashSet

--- section_21/src/Main.java
+++ section_21/src/Main.java
@@ -1,5 +1,34 @@
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(removeVowelWords());;
     }
+
+    public static Set<String> removeVowelWords () {
+
+        Set<String> fruits = new HashSet<>();
+        fruits.add("apple");
+        fruits.add("banana");
+        fruits.add("orange");
+        fruits.add("lynx");
+        fruits.add("grape");
+
+        Iterator<String> it = fruits.iterator();
+
+        while (it.hasNext()) {
+
+            if (it.next().matches("[a-u,A-U]*")) {
+                it.remove();
+            }
+
+        }
+
+        return fruits;
+
+    }
+
 }
