637f14ff4f5ca5db61336e245df45992499f4dde
Anderson Sipriano <Andsipriano@gmail.com>
2024-07-04 19:23:58 -0300

    Introduction to Enums

---
+++ section_22/src/com/sipriano/enumeration/TaskPriorityConstantDemo.java
@@ -0,0 +1,13 @@
+package com.sipriano.enumeration;
+
+public class TaskPriorityConstantDemo {
+
+    public static void main(String[] args) {
+        System.out.println(TaskUtil.getEstimatedCompletionTime(0));
+        System.out.println(TaskUtil.getEstimatedCompletionTime(1));
+        System.out.println(TaskUtil.getEstimatedCompletionTime(2));
+        System.out.println(TaskUtil.getEstimatedCompletionTime(3));
+        System.out.println(TaskUtil.getEstimatedCompletionTime(6));
+    }
+
+}
