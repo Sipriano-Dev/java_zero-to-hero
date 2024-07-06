516228a21a7ed00b154af4a1ee3a9bc9824c9f3c
Anderson Sipriano <Andsipriano@gmail.com>
2024-07-06 18:13:22 -0300

    Demo of java.util.Date - Part 2

--- section_23/src/com/sipriano/legacy/JavaUtilDate.java
+++ section_23/src/com/sipriano/legacy/JavaUtilDate.java
@@ -6,6 +6,7 @@ public class JavaUtilDate {
 
     public static void main(String[] args) {
         Date currentDate = new Date();//Current date and time value
+        //currentDate.setYear(161); //Deprecated
         System.out.println(currentDate);
 
         long timeInMs = currentDate.getTime();
@@ -14,6 +15,14 @@ public class JavaUtilDate {
         Date futureDate = new Date(160, 9, 14);//1900 + 160, month -1
         Date futureDateTime = new Date(160, 9, 14, 8, 14, 34);
         System.out.println(futureDateTime);//Thu Oct 14 00:00:00 BRT 2060
+
+        long timeInMs1 = futureDate.getTime();
+        Date futureDate2 = new Date(timeInMs1+86400000);
+        System.out.println(futureDate2);
+
+        boolean isAfter = futureDate.after(futureDate2);
+        System.out.println(isAfter);
+
 
         /*
         In computing, an epoch is a fixed date and time used as a reference from
