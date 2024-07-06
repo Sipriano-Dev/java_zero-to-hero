79bcdd760b183972fe9a82a6e07738b3545881e1
Anderson Sipriano <Andsipriano@gmail.com>
2024-07-06 17:51:13 -0300

    Why a new Date API introduced in Java 8

---
+++ section_23/src/com/sipriano/legacy/LegacyDateApiDemo.java
@@ -0,0 +1,13 @@
+package com.sipriano.legacy;
+
+import java.util.Date;
+
+public class LegacyDateApiDemo {
+
+    public static void main(String[] args) {
+        Date date = new Date();
+        Date date1 = new Date(124, 8, 26);
+        System.out.println(date1);
+    }
+
+}
