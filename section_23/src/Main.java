78f8473a4bf3d6faf4b484956476c191e0bccd79
Anderson Sipriano <Andsipriano@gmail.com>
2024-07-16 16:36:48 -0300

    [JAVA 8] Demo of Instant and Duration in java.time Date and Time API

--- section_23/src/Main.java
+++ section_23/src/Main.java
@@ -1,5 +1,31 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(localDateOperations());
     }
+
+    public static LocalDate localDateOperations() {
+        LocalDate date = LocalDate.of(2025, 12, 25);
+        LocalDate newDate = date.plusMonths(6).plusDays(18);
+        return newDate;
+    }
+
+    public static LocalTime localTimeOperations() {
+        LocalTime time = LocalTime.of(9, 0, 0);
+        LocalTime newTime = time.plusHours(8).plusMinutes(15);
+        return newTime;
+    }
+
+    public static LocalDateTime localDateTimeOperations() {
+        LocalDateTime dateTime = LocalDateTime.of(2025, 12, 25, 9, 30, 0);
+        LocalDateTime newDateTime =dateTime.minusWeeks(4).minusMinutes(30);
+        return newDateTime;
+    }
+
+
 }
