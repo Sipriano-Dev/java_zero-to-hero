9f4b89ad90c53f35b039fea3e8c93e0d73b1a64b
Anderson Sipriano <Andsipriano@gmail.com>
2024-07-19 17:30:31 -0300

    [JAVA 8] Demo of ZoneOffset

--- section_23/src/com/sipriano/newapi/ZoneDateTimeDemo.java
+++ section_23/src/com/sipriano/newapi/ZoneDateTimeDemo.java
@@ -2,6 +2,7 @@ package com.sipriano.newapi;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 
 public class ZoneDateTimeDemo {
@@ -9,11 +10,23 @@ public class ZoneDateTimeDemo {
     public static void main(String[] args) {
         ZoneId zone = ZoneId.of("Australia/Melbourne");
         ZoneId destZone = ZoneId.of("America/Fortaleza");
+
         ZonedDateTime australiaDateTime = ZonedDateTime.now(zone);
         ZonedDateTime americaDateTime = australiaDateTime.withZoneSameInstant(destZone);
 
         System.out.println(australiaDateTime);
         System.out.println(americaDateTime);
+
+        //ZoneOffset offset = ZoneOffset.ofHoursMinutes(5,30);
+        ZoneOffset offset = ZoneOffset.ofHours(2);
+        ZoneOffset destOffset = ZoneOffset.ofHours(-8);
+
+        ZonedDateTime utc2DateTime = ZonedDateTime.now(offset);
+        ZonedDateTime utcDestDateTime = australiaDateTime.withZoneSameInstant(destOffset);
+
+        System.out.println(utc2DateTime);
+        System.out.println(utcDestDateTime);
+
 
     }
 
