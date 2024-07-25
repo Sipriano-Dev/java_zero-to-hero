f21ddedae4dddeeaea18f9e0f9a0419c7a8386d3
Anderson Sipriano <Andsipriano@gmail.com>
2024-07-25 16:50:07 -0300

    How Functional Interface & Lambda expressions are linked - Part 2

--- section_24/src/com/sipriano/lambda/LambdaExpressionDemo.java
+++ section_24/src/com/sipriano/lambda/LambdaExpressionDemo.java
@@ -7,9 +7,23 @@ public class LambdaExpressionDemo {
         //Functional interface have only 1 method, that way, it will create an object and guess
         //everything behind the scenes
         Hello hello = () -> System.out.println("Hello There");
-        process(hello);
+        process(hello); //Approach 1
 
-        process(() -> System.out.println("Hello There again"));
+        process(() -> System.out.println("Hello There again")); //Approach 2 - Direct
+
+//        ArithmeticOperation addition = (a, b) -> {
+//            int result = a + b;
+//            return result;
+//        };
+        ArithmeticOperation addition = (a, b)->a+b;
+        ArithmeticOperation subtraction = (a, b)->a-b;
+        ArithmeticOperation product = (a, b)->a*b;
+        ArithmeticOperation division = (a, b)->a/b;
+
+        System.out.println(addition.operation(5, 3));
+        System.out.println(subtraction.operation(5, 3));
+        System.out.println(product.operation(5, 3));
+        System.out.println(division.operation(5, 3));
     }
 
     public static void process(Hello h) {
