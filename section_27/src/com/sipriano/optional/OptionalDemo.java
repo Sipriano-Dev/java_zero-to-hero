c79196c301df41301414d49011b9a28182e823e9
Anderson Sipriano <Andsipriano@gmail.com>
2024-08-13 15:27:17 -0300

    Demo of Optional - Part 2

--- section_27/src/com/sipriano/optional/OptionalDemo.java
+++ section_27/src/com/sipriano/optional/OptionalDemo.java
@@ -1,5 +1,6 @@
 package com.sipriano.optional;
 
+import java.util.Optional;
 import java.util.Random;
 
 public class OptionalDemo {
@@ -7,18 +8,47 @@ public class OptionalDemo {
     public static void main(String[] args) {
 
         //Without Optional you have to handle possible null exception like this
-        String msg = sayHello();
-        if (msg != null) {
-            System.out.println(msg.toUpperCase());
+        String message = sayHi();
+        if (message != null) {
+            System.out.println(message.toUpperCase());
         }
+
+        //Using Optional the course way
+        Optional<String> optionalMsg = sayHello();
+        if (optionalMsg.isPresent()) {
+            System.out.println(optionalMsg.get().toUpperCase());
+        }
+
+        //Another way
+        optionalMsg.ifPresent(x -> System.out.println(x.toUpperCase()));
+
 
     }
 
-    public static String sayHello() {
+    public static Optional<String> sayHello() {
+        int num = new Random().nextInt();
+        System.out.println(num);
+        String msg = null;
+        if (num % 2 == 0) {
+            msg = "Hello World!";
+        }
+        return Optional.ofNullable(msg);
+    }
+
+//    public static Optional<String> sayHello() {
+//        int num = new Random().nextInt();
+//        System.out.println(num);
+//        if (num % 2 == 0) {
+//            return Optional.of("Hello World!");
+//        }
+//        return Optional.empty();
+//    }
+
+    public static String sayHi() {
         int num = new Random().nextInt();
         System.out.println(num);
         if (num % 2 == 0) {
-            return "Hello World!";
+            return "Hi World!";
         }
         return null;
     }
